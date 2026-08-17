de8cc281704d25089fc775b1e81e494129d33d9d
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-08-17 18:29:42 -0300

    Importo Script simple para movimiento con W A S D

---
+++ Assets/Scripts/Movement/WASDmovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WASDmovement : MonoBehaviour
+{
+    [SerializeField] private float _maxSpeed = 5;
+    private Vector3 _velocity;
+
+
+
+    void Update()
+    {
+        transform.position += _velocity * Time.deltaTime;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 movement = value.Get<Vector2>();
+        _velocity = new Vector3(movement.x, 0, movement.y) * _maxSpeed;
+    }
+}
