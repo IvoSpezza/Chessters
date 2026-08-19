54571a77d2c8d68dd0cd5f5ddb99acd47063e266
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-08-19 20:32:36 -0300

    actualizando el movimiento wasd del player
    anñadiendo correr

--- Assets/Scripts/Movement/WASDmovement.cs
+++ Assets/Scripts/Movement/WASDmovement.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.PlayerSettings;
 
 public class WASDmovement : MonoBehaviour
 {
     [SerializeField] private float _maxSpeed = 5;
     private Vector3 _velocity;
 
-
+    private bool _isRunning;
 
     void Update()
     {
+        Debug.Log(_isRunning);
         transform.position += _velocity * Time.deltaTime;
     }
 
@@ -18,4 +20,11 @@ public class WASDmovement : MonoBehaviour
         Vector2 movement = value.Get<Vector2>();
         _velocity = new Vector3(movement.x, 0, movement.y) * _maxSpeed;
     }
+
+    public void OnSprint(InputValue value)
+    {
+        _isRunning = value.isPressed;
+        Debug.Log("POllo");
+    }
+
 }
